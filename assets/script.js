49a6c361467c8498adb1fc1561d3a876b27d0bf5
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-01 17:57:25 -0500

    added applyColors function to toggle class of past present future depending on hour, currently not working

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,15 @@
 // in the html.
 
 $(function () {
+  var currentHour = dayjs().format("H");
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  // when user enters text and clicks save button, value is stored in local storage
   $(".saveBtn").on("click", function () {
     // variable to store the id of the specific parent you target (this) using its id (hour)
     var parentId = $(this).parent().attr("id");
@@ -18,16 +21,36 @@ $(function () {
     console.log(childId);
     localStorage.setItem(parentId, childId);
   });
-  
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  function applyColors() {
+    // targeting each time-block object
+    $("time-block").each(function () {
+      // referenced mdn article on parseInt in order to parse just the hour number
+      var hourCard = parseInt(this.id);
+      // toggles the classes past, present, or future depending on the current hour vs the hour card id
+      $(this).toggleClass("past", hourCard < currentHour);
+      $(this).toggleClass("present", hourCard === currentHour);
+      $(this).toggleClass("future", hourCard < currentHour);
+    });
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  function saveText() {}
   // TODO: Add code to display the current date in the header of the page.
+
+  // displays current date with day of the week in header
+  function headerDate() {
+    var dateEl = $("#currentDay");
+    var currentDate = dayjs().format("dddd, MMMM D");
+    dateEl.text(currentDate);
+  }
+
+  headerDate();
+  applyColors();
 });
